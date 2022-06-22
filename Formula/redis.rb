0faeb6bf1ea41196dbf2b12e0f4f28045ff36dc1
Bang Nguyen <luongbang152@gmail.com>
2022-06-22 16:37:05 +0000

    redis: update 6.2.6 bottle.

--- Formula/redis.rb
+++ Formula/redis.rb
@@ -12,13 +12,9 @@ class Redis < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_monterey: "a656500c3b5762c7cfe03d587a4fa08c5df4568783d167555962d850e8cab3c3"
-    sha256 cellar: :any,                 arm64_big_sur:  "846aada68ca07b36d58fd620ed5d52ae67a759526c5da27042748363bfdb6271"
-    sha256 cellar: :any,                 monterey:       "ac30519a604ff014e3903893ddca6c563c134002fec58df3613632e42c4d117c"
-    sha256 cellar: :any,                 big_sur:        "246f73498993a2a0c6c4326a298d2fcc3da6d61904ad09a631aa9c63a6800f76"
-    sha256 cellar: :any,                 catalina:       "ff93a763d622cc9130c09fa9ce2ec7236f91562667eaa5c304fcf175c1253746"
-    sha256 cellar: :any,                 mojave:         "57842762aad1434f8b511f603364b4528f0545f7d768c9387b362011351cda2b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8398fc05ef8eb1ea3d7b26844e3a314a948b3d0d4fb937a00c6c62f0abbe340a"
+    root_url "https://github.com/luongbang152/homebrew-brew/releases/download/redis-6.2.6"
+    sha256 cellar: :any,                 big_sur:      "9219cb2ad32149c70421793a837a08c872919fbb272130680ef04bc9affe60dc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "5dada42f07c7ed32c9bd84b51871557401489655e68126854cebb01c69fb2650"
   end
 
   depends_on "openssl@1.1"
